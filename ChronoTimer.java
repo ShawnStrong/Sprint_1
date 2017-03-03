8d9bc165bf5851e61435c5774148af8bcac3bd2e
dphan1994 <dphan7743@gmail.com>
2017-03-02 21:27:32 -0600

    Update ChronoTimer.java

--- ChronoTimer.java
+++ ChronoTimer.java
@@ -3,96 +3,111 @@ package home;
 import java.util.Scanner;
 
 public class ChronoTimer {
+	
 	static boolean power;
 	static Scanner console = new Scanner(System.in);
+	boolean chanTog [] = new boolean[9];
+	static int totRacers;
+	static Racer [] racers = new Racer[5];
 	
-	public ChronoTimer()
-	{
+	public ChronoTimer() {
+		
 		ChronoTimer.power = false; 
 	}
 	
-	public static void main(String args[])
-	{
+	public static void main (String args[]) {
+		
 		Simulator Chrono = new Simulator();
 		Chrono.textReader();
 	}
 	
-	static void getInput()
-	{
-		System.out.println("Enter Your command: 'LIST' for list of commands: ");
-		String input;
+	static void getInput() {
+		
 		boolean runConsole = true;
-		while(runConsole) 
-		{
+		
+		while(runConsole) {
+			
+			System.out.println("Enter Your command: 'LIST' for list of commands: ");
+			String input;
 			input = console.nextLine();
+			String [] splitted = input.split("\\s+");
 				
-			if (input.equalsIgnoreCase("POWER"))
-			{
-/////// adding power call, terminates if power called ???? change???
+			if (input.equalsIgnoreCase("POWER")) {
+				/////// adding power call, terminates if power called ???? change???
 				System.exit(0);
 			}
 				
-				
-			else if(input.equalsIgnoreCase("EXIT"))
-			{
+			else if (input.equalsIgnoreCase("EXIT")) {
 				
 				
-			}
-			else if(input.equalsIgnoreCase("TIME"))
-			{
-				
-				
-			}
-			else if(input.equalsIgnoreCase("RESET"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("DNF"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("CANCEL"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("TOG"))
-			{
-				System.out.println("toggled: next command?");
-			}
-			else if(input.equalsIgnoreCase("TRIG"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("START"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("FINISH"))
-			{
-				
-			}
-			else if(input.equalsIgnoreCase("LIST"))
-			{
-				System.out.println("POWER: end program. \n" + "EXIT: \n" +
-			"RESET: reset all run times and settings \n" + "TIME: \n" + "DNF: end that racers run, with no finish time\n"
-			+ "...................... \n");
-			}
-			else
-			{
-				System.out.println("Not a valid command, type 'LIST' to list commands: \n");
 			}
 			
-		
+			else if (input.equalsIgnoreCase("TIME")) {
+				
+				
+			}
+			
+			else if (input.equalsIgnoreCase("RESET")) {
+				
+			}
+			
+			else if (input.equalsIgnoreCase("DNF")) {
+				
+			}
+			
+			else if (input.equalsIgnoreCase("CANCEL")) {
+				
+				
+			}
+			
+			else if (input.equalsIgnoreCase("TOG")) {
+				
+				System.out.println("toggled: next command?");
+			}
+			
+			else if (input.equalsIgnoreCase("TRIG")) {
+				
+				
+			}
+			
+			else if (input.equalsIgnoreCase("START")) {
+				
+			}
+			
+			else if (input.equalsIgnoreCase("FINISH")) {
+				
+			}
+			
+			else if (splitted[0].equalsIgnoreCase("NUM")) {
+				
+				totRacers++;
+				racers[totRacers] = new Racer (Integer.parseInt(splitted[1]), totRacers);
+			}
+			
+			else if (input.equalsIgnoreCase("LIST")) {
+				
+				System.out.println("POWER: end program. \n" + 
+						"EXIT: \n" +
+						"RESET: reset all run times and settings \n" + "TIME: \n" + 
+						"DNF: end that racers run, with no finish time\n" + 
+						"NUM: to create racer\n" + 
+						"...................... \n");
+			}
+			
+			else {
+				System.out.println("Not a valid command, type 'LIST' to list commands: \n");
+			}	
 		}//end while
 	}
 	
 	
-	static boolean getPower()
-	{
+	static boolean getPower() {
+		
 		return power;
 	}
-	static boolean setPower()
-	{
+	
+	static boolean setPower() {
+		
 		if(!power)
 		{
 			power = true;
