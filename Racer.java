87be7e8fc040a505d6d46d01fa0c0817748748da
Andrew <blizzard383@ymail.com>
2017-03-03 20:57:07 -0600

    Update Racer.java

--- Racer.java
+++ Racer.java
@@ -1,15 +1,15 @@
 package home;
 
 public class Racer {
-	
 	public int racerNum;
 	public int totalRacers;
 	public int start;
 	public int fin;
+	public int index;
 	public int x;
 	public int y;
 	
-	public Racer (int rNum, int tRacers) {
+	public Racer (int rNum, int tRacers, int channel) {
 		
 		this.racerNum = rNum;
 		this.totalRacers = tRacers;
@@ -19,5 +19,7 @@ public class Racer {
 		this.y = totalRacers * 2;
 		this.start = 0;
 		this.fin = 0;
+		this.index = channel;
 	}
+
 }
