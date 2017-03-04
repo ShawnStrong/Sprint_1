5230c1b13b6a88cf987a813350346010bf42f64e
Andrew <blizzard383@ymail.com>
2017-03-03 20:33:00 -0600

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,31 @@
+package home;
+
+import java.time.Clock;
+import java.time.LocalTime;
+
+public class Time {
+	static Clock time;
+	static LocalTime time2;
+	
+	public Time()
+	{
+		time = Clock.systemDefaultZone();
+		//time = Clock.systemUTC();
+		
+	}
+	
+	public double getRacerTime()
+	{
+		return time.millis();
+	}
+	public LocalTime getSystemTime()
+	{
+		return LocalTime.now();
+	}
+	public double calcTime(int start, int fin)
+	{
+		return (fin-start) / 100;
+		//System.out.println(LocalTime.now());
+		
+	}
+}
