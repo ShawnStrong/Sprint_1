c569cc820a6f31e4d62cf409e1300374862df8d4
DilK27 <dil.khahra@gmail.com>
2017-03-05 14:54:36 -0600

    Update ChronoTimer.java

--- ChronoTimer.java
+++ ChronoTimer.java
@@ -206,7 +206,7 @@ public class ChronoTimer {
 		for(int i = 0; i < completed.size(); i++) {
 			
 			Racer temp = completed.get(i);
-			System.out.println("\nRacer " + temp.racerNum + " time: " + stopWatch.formatTime((temp.fin - temp.start)));
+			System.out.println("\nRacer " + temp.racerNum + " time: " + stopWatch.formatTime((temp.fin - temp.start))+ "\n");
 		}
 	}
 	
